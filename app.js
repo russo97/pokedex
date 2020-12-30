99940dd1dea312988b19778a7e7b60677d7b6d72
Wanderson Silva <wandersonsilva768@gmail.com>
2020-12-30 03:15:37 -0300

    feat: exibindo resultados da requisição na UL

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
 
+Vue.component('pokemon-item', {
+  props: {
+    name: {
+      type: String
+    }
+  },
 
-console.log(Vue);
+  template: `
+    <li class="card">
+      {{ po }}
+    </li>
+  `
+});
+
+
+new Vue({
+  el: "#pokedex",
+
+  data () {
+    return {
+      allPokemons: [],
+      baseURL: 'https://pokeapi.co/api/v2/pokemon/'
+    }
+  },
+
+  mounted: async function () {
+    const { baseURL } = this;
+
+    const pokemonList = Array.from({ length: 150 }, () => null);
+
+    const basedURL = index => `${baseURL}${index}`;
+
+    const mappedpokemonList = await pokemonList.map((_, index) => {
+      return fetch(basedURL((index + 1))).then(res => res.json()).catch(err => console.log(err));
+    });
+
+    this.allPokemons = await Promise.all(mappedpokemonList);
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+    eachPokemon () {
+      return 'ok';
+    }
+  },
+
+  watch: {}
+});
 
