57fb011da14f73ceae868c9a419f48af0d10fd5c
Wanderson Silva <wandersonsilva768@gmail.com>
2020-12-30 04:06:24 -0300

    feat: adicionado componente para exibição de cada pokémon

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 
-Vue.component('pokemon-item', {
-  props: {
-    name: {
-      type: String
+const poke = Vue.component('pokemon-item', {
+  name: "PokemonItem",
+
+  props: ['pokemon'],
+
+  computed: {
+    name () {
+      return this.pokemon.name;
+    },
+
+    pokemonThumb () {
+      const { sprites } = this.pokemon;
+
+      return sprites.other.dream_world.front_default;
+    },
+
+    types () {
+      const { pokemon } = this;
+
+      return pokemon.types.map(inf => inf.type.name);
     }
   },
 
   template: `
-    <li class="card">
-      {{ po }}
+    <li class="card" :class="types[0]">
+      <img :src="pokemonThumb" :alt="name" class="card-image" />
+      <h1 class="card-title"> {{ name }} </h1>
+      <h6 class="card-subtitle">
+        {{ types.join(' - ') }}
+      </h6>
     </li>
   `
 });
@@ -25,26 +45,30 @@ new Vue({
   },
 
   mounted: async function () {
-    const { baseURL } = this;
+    const { basedURL, fetchError } = this;
 
     const pokemonList = Array.from({ length: 150 }, () => null);
 
-    const basedURL = index => `${baseURL}${index}`;
+    const mappedPokemonList = await pokemonList.map((_, index) => {
+      const pIndex = index + 1;
 
-    const mappedpokemonList = await pokemonList.map((_, index) => {
-      return fetch(basedURL((index + 1))).then(res => res.json()).catch(err => console.log(err));
+      return fetch(basedURL(pIndex))
+        .then(res => res.json())
+        .catch(fetchError);
     });
 
-    this.allPokemons = await Promise.all(mappedpokemonList);
+    this.allPokemons = await Promise.all(mappedPokemonList);
   },
 
   methods: {
+    basedURL (index) {
+      const { baseURL } = this;
 
-  },
+      return `${baseURL}${index}`;
+    },
 
-  computed: {
-    eachPokemon () {
-      return 'ok';
+    fetchError (error) {
+      console.error(`ERROR: ${error}`);
     }
   },
 
